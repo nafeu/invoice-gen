cf935bccccbc7591446f3696bf367e281be9c50f
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-12-04 21:33:14 -0500

    Implement arg parsing and file handling

--- invoice-gen.py
+++ invoice-gen.py
@@ -1,26 +1,73 @@
 import pdfkit
+import json
+import subprocess
+import argparse
+import os.path
+
+CONFIG_PATH = "config.json"
+DEFAULT_CONFIG = {
+  "name": "[BUSINESS_NAME]",
+  "address": "[BUSINESS_ADDRESS]",
+  "city_province_country": "[BUSINESS_CITY_PROVINCE_COUNTRY]",
+  "postal_code": "[BUSINESS_POSTAL_CODE]",
+  "phone": "[BUSINESS_PHONE]",
+  "email": "[BUSINESS_EMAIL]",
+  "invoice_type": "invoice",
+  "clients": [
+    {
+      "id": 1,
+      "name": "[CLIENT_NAME]",
+      "address": "[CLIENT_ADDRESS]",
+      "postal": "[CLIENT_POSTAL]",
+      "phone": "[CLIENT_PHONE]"
+    }
+  ]
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument("clients", nargs='?')
+parser.add_argument("-b", "--build",
+                    help="build a new pdf",
+                    metavar='JSON',
+                    nargs=1)
+
+args = parser.parse_args()
 
 def main():
-  config = {
-    "name": "Nafeu Nasir Media Solutions",
-    "address": "EXAMPLE ADDRESS",
-    "city_province_country": "Toronto, Ontario, Canada",
-    "postal_code": "??????",
-    "phone": "+1(111)-111-1111",
-    "invoice_type": "invoice",
-  }
-  with open("templates/index.html", "r") as file:
-    print("Generating...")
-    template = file.read()
-    template = template.replace("[BUSINESS_NAME]", config['name']) \
-                       .replace("[BUSINESS_ADDRESS]", config['address']) \
-                       .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
-                       .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
-                       .replace("[BUSINESS_PHONE]", config['phone']) \
-                       .replace("[INVOICE_TYPE]", config['invoice_type'], 2) \
-                       .replace("[INVOICE_ITEMS]", getInvoiceItems())
-    pdfkit.from_string(template, 'export.pdf')
-    print("Complete.")
+  config = {}
+  # if (os.path.isfile(args.build[0])):
+  #   with open(args.build[0]) as raw_config:
+  #     config = json.load(raw_config)
+
+  if (os.path.isfile(CONFIG_PATH)):
+    with open(CONFIG_PATH) as raw_config:
+      config = json.load(raw_config)
+  else:
+    with open(CONFIG_PATH, 'w') as file:
+      json.dump(DEFAULT_CONFIG, file, indent=2)
+    print("Missing config, please fill it in and try again.")
+    subprocess.check_call(["open", CONFIG_PATH])
+    exit()
+
+  if (args.clients):
+    for client in config['clients']:
+      print("%s - %s" % (client['id'], client['name']))
+  elif (args.build):
+    with open("templates/index.html", "r") as file:
+      print("Generating...")
+      template = file.read()
+      template = template.replace("[BUSINESS_NAME]", config['name']) \
+                         .replace("[BUSINESS_ADDRESS]", config['address']) \
+                         .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
+                         .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
+                         .replace("[BUSINESS_PHONE]", config['phone']) \
+                         .replace("[INVOICE_TYPE]", config['invoice_type'], 2) \
+                         .replace("[INVOICE_ITEMS]", getInvoiceItems())
+      pdfkit.from_string(template, 'export.pdf')
+      subprocess.check_call(["open", "-a", "Preview.app", "export.pdf"])
+      print("Complete.")
+  else:
+    print(args)
 
 def getInvoiceItems():
     items = [
