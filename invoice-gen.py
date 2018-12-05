df3abc2fa33fdf313a3d67cd0b9c45a74fdb5848
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-12-05 04:08:12 -0500

    Abstract main logic into functions and add implement appropriate argument logic

--- invoice-gen.py
+++ invoice-gen.py
@@ -3,6 +3,9 @@ import json
 import subprocess
 import argparse
 import os.path
+import datetime
+
+now = datetime.datetime.now()
 
 CONFIG_PATH = "config.json"
 DEFAULT_CONFIG = {
@@ -12,91 +15,114 @@ DEFAULT_CONFIG = {
   "postal_code": "[BUSINESS_POSTAL_CODE]",
   "phone": "[BUSINESS_PHONE]",
   "email": "[BUSINESS_EMAIL]",
-  "invoice_type": "invoice",
-  "clients": [
+  "abrv": "[ABRV]",
+  "customers": [
     {
       "id": 1,
-      "name": "[CLIENT_NAME]",
-      "address": "[CLIENT_ADDRESS]",
-      "postal": "[CLIENT_POSTAL]",
-      "phone": "[CLIENT_PHONE]"
+      "name": "[CUSTOMER_NAME]",
+      "address": "[CUSTOMER_ADDRESS]",
+      "city_province_country": "[CUSTOMER_CITY_PROVINCE_COUNTRY]",
+      "postal_code": "[CUSTOMER_POSTAL_CODE]",
+      "phone": "[CUSTOMER_PHONE]"
+    }
+  ]
+}
+DEFAULT_DATA = {
+  "customer_id": 0,
+  "invoice_type": "invoice",
+  "invoice_date": now.strftime("%b %m, %Y"),
+  "invoice_number": DEFAULT_CONFIG['abrv'] + str(now.strftime("%d%m%Y")),
+  "items": [
+    {
+      "desc": "[DESCRIPTION]",
+      "hours": 0,
+      "rate": 35,
+      "total": 0
     }
   ]
 }
 
-parser = argparse.ArgumentParser()
-parser.add_argument("clients", nargs='?')
+parser = argparse.ArgumentParser(description='Generate invoices.')
+parser.add_argument('customer_id', nargs='?', const=0, help="enter customer id to initiatlize invoice")
+parser.add_argument("-l", "--list-customers", help="list all customers", action='store_true')
 parser.add_argument("-b", "--build",
-                    help="build a new pdf",
-                    metavar='JSON',
+                    help="build a new pdf using the specified json file",
+                    metavar='JSON_FILE',
                     nargs=1)
 
 args = parser.parse_args()
 
 def main():
   config = {}
-  # if (os.path.isfile(args.build[0])):
-  #   with open(args.build[0]) as raw_config:
-  #     config = json.load(raw_config)
-
   if (os.path.isfile(CONFIG_PATH)):
     with open(CONFIG_PATH) as raw_config:
       config = json.load(raw_config)
   else:
-    with open(CONFIG_PATH, 'w') as file:
-      json.dump(DEFAULT_CONFIG, file, indent=2)
+    create_json_file(CONFIG_PATH, DEFAULT_CONFIG)
     print("Missing config, please fill it in and try again.")
-    subprocess.check_call(["open", CONFIG_PATH])
+    open_file(CONFIG_PATH)
     exit()
+  if args.list_customers:
+    list_customers(config)
+  elif args.build:
+    data = {}
+    if (os.path.isfile(args.build[0])):
+      with open(args.build[0]) as raw_data:
+        data = json.load(raw_data)
+      build_pdf(config, data)
+    else:
+      print("Invoice data file '%s' does not exist." % args.build[0])
+  elif len([(customer['id'] == args.customer_id) for customer in config['customers']]) > 0:
+    create_json_file(args.customer_id + "_invoice.json", DEFAULT_DATA)
+    open_file(args.customer_id + "_invoice.json")
 
-  if (args.clients):
-    for client in config['clients']:
-      print("%s - %s" % (client['id'], client['name']))
-  elif (args.build):
-    with open("templates/index.html", "r") as file:
-      print("Generating...")
-      template = file.read()
-      template = template.replace("[BUSINESS_NAME]", config['name']) \
-                         .replace("[BUSINESS_ADDRESS]", config['address']) \
-                         .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
-                         .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
-                         .replace("[BUSINESS_PHONE]", config['phone']) \
-                         .replace("[INVOICE_TYPE]", config['invoice_type'], 2) \
-                         .replace("[INVOICE_ITEMS]", getInvoiceItems())
-      pdfkit.from_string(template, 'export.pdf')
-      subprocess.check_call(["open", "-a", "Preview.app", "export.pdf"])
-      print("Complete.")
-  else:
-    print(args)
+def list_customers(config):
+  for customer in config['customers']:
+    print("%s - %s" % (customer['id'], customer['name']))
 
-def getInvoiceItems():
-    items = [
-      {
-        "desc": "Completed such and such work and did this and that",
-        "hours": 40,
-        "rate": 25,
-        "total": 1000.00
-      },
-      {
-        "desc": "Did some stuff here and there",
-        "hours": 40,
-        "rate": 25,
-        "total": 1000.00
-      },
-      {
-        "desc": "Did some other stuff here and there",
-        "hours": 40,
-        "rate": 25,
-        "total": 1000.00
-      }
-    ]
-    out = ""
-    for item in items:
-      out += "<tr><td>%s</td>" % item['desc']
-      out += "<td class='text-right'>%s</td>" % item['hours']
-      out += "<td class='text-right'><span class='currency'>$</span>%s</td>" % item['rate']
-      out += "<td class='text-right'><span class='currency'>$</span>%s</td></tr>" % item['total']
-    return out
+def build_pdf(config, data):
+  with open("templates/index.html", "r") as file:
+    print("Generating...")
+    customer = get_customer_by_id(config['customers'], data['customer_id'])
+    template = file.read()
+    template = template.replace("[BUSINESS_NAME]", config['name']) \
+                       .replace("[BUSINESS_ADDRESS]", config['address']) \
+                       .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
+                       .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
+                       .replace("[BUSINESS_PHONE]", config['phone']) \
+                       .replace("[CUSTOMER_NAME]", customer['name']) \
+                       .replace("[CUSTOMER_ADDRESS]", customer['address']) \
+                       .replace("[CUSTOMER_CITY_PROVINCE_COUNTRY]", customer['city_province_country']) \
+                       .replace("[CUSTOMER_POSTAL_CODE]", customer['postal_code']) \
+                       .replace("[INVOICE_DATE]", data['invoice_date']) \
+                       .replace("[INVOICE_NUMBER]", data['invoice_number']) \
+                       .replace("[INVOICE_TYPE]", data['invoice_type'], 2) \
+                       .replace("[INVOICE_ITEMS]", get_invoice_items(data))
+    pdfkit.from_string(template, 'export.pdf')
+    subprocess.check_call(["open", "-a", "Preview.app", "export.pdf"])
+    print("Complete.")
+
+def open_file(path):
+  subprocess.check_call(["open", path])
+
+def create_json_file(path, data):
+  with open(path, 'w') as file:
+    json.dump(data, file, indent=2)
+
+def get_customer_by_id(customers, customer_id):
+  for customer in customers:
+    if customer['id'] == customer_id:
+      return customer
+  return None
+
+def get_invoice_items(data):
+  out = ""
+  for item in data['items']:
+    out += "<tr><td>%s</td>" % item['desc']
+    out += "<td class='text-right'>%s</td>" % item['hours']
+    out += "<td class='text-right'><span class='currency'>$</span>%s</td>" % item['rate']
+    out += "<td class='text-right'><span class='currency'>$</span>%s</td></tr>" % item['total']
+  return out
 
 if __name__ == '__main__':
   main()
