a36d6ae78b297691b4d205155d5f19d15a8884f4
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-12-07 14:26:08 -0500

    Add default template right into script

--- invoice-gen.py
+++ invoice-gen.py
@@ -11,6 +11,7 @@ import random
 now = datetime.datetime.now()
 
 CONFIG_PATH = "config.json"
+INVOICE_NUMBER_UID_LENGTH = 5
 DEFAULT_CONFIG = {
     "name": "[BUSINESS_NAME]",
     "address": "[BUSINESS_ADDRESS]",
@@ -44,7 +45,178 @@ DEFAULT_DATA = {
         }
     ]
 }
-INVOICE_NUMBER_UID_LENGTH = 5
+DEFAULT_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Invoice Gen</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Titillium+Web:200,300,400,600,700" rel="stylesheet">
+  <style type="text/css">
+    body {
+      font-family: "Titillium Web", "Source Sans Pro", Apple SD Gothic Neo, Nanum Barun Gothic, Nanum Gothic, Verdana, Arial, Dotum, sans-serif;
+      font-size: 1.2em;
+      color: #343a40;
+      background-color: #fff;
+      -webkit-font-smoothing: antialiased;
+      padding-top: 50px;
+    }
+
+    p {
+      font-weight: 300;
+    }
+
+    .title {
+      font-weight: 600;
+      font-size: 1.7em;
+    }
+
+    td, .thin {
+      font-weight: 300;
+    }
+
+    .seperator {
+      border-bottom: 1px solid black;
+      width: 500px;
+    }
+
+    .invoice-type {
+      background-color: black;
+      color: white;
+      text-align: left;
+    }
+
+    .invert-color {
+      background-color: black;
+      color: white;
+    }
+
+    .col-half {
+      width: 50%;
+      display: inline-block;
+      float: left;
+      height: 175px;
+      margin-top: 15px;
+    }
+
+    .bb-none {
+      border-bottom: none !important;
+    }
+
+    .bt-none {
+      border-top: none !important;
+    }
+
+    .bl-none {
+      border-left: none !important;
+    }
+
+    .br-none {
+      border-right: none !important;
+    }
+
+    .text-small {
+      font-size: 0.75em;
+    }
+
+    .currency {
+      float: left;
+    }
+
+    table {
+      font-size: 0.9em;
+    }
+
+    .sep {
+      margin: 15px 0px 30px 0px;
+      width: 100%;
+      height: 2px;
+      background-color: #DEE2E6;
+    }
+
+    .bold {
+      font-weight: 700;
+    }
+
+    .item-cell {
+      border: none !important;
+    }
+  </style>
+</head>
+<body>
+  <div class="row justify-content-center text-center mt-5">
+    <div class="col-md-12 mb-4">
+      <div class="title">[BUSINESS_NAME]</div>
+      <div class="thin">[BUSINESS_ADDRESS]</div>
+      <div class="thin">[BUSINESS_CITY_PROVINCE_COUNTRY]</div>
+      <div class="thin">[BUSINESS_POSTAL_CODE]</div>
+      <div class="thin text-small">[BUSINESS_PHONE]</div>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-sm-10 mb-4">
+      <div class="text-justify text-uppercase thin pl-2 invoice-type mb-2">[INVOICE_TYPE]</div>
+      <div class="col-half">
+        <h4>BILL TO:</h4>
+        <div class="thin">[CUSTOMER_NAME]</div>
+        <div class="thin">[CUSTOMER_ADDRESS]</div>
+        <div class="thin">[CUSTOMER_CITY_PROVINCE_COUNTRY]</div>
+        <div class="thin">[CUSTOMER_POSTAL_CODE]</div>
+      </div>
+      <div class="col-half">
+        <div><span class="thin">DATE:</span> [INVOICE_DATE]</div>
+        <div><span class="thin text-uppercase">[INVOICE_TYPE] #:</span> [INVOICE_NUMBER]</div>
+        <div><span class="thin">CUSTOMER ID:</span> [CUSTOMER_ID]</div>
+      </div>
+    </div>
+    <div class="col-sm-10">
+      <table class="table table-bordered">
+        <tr>
+          <th>Description</th>
+          <th>Hours</th>
+          <th>Pay Rate</th>
+          <th>Amount (CAD)</th>
+        </tr>
+        [INVOICE_ITEMS]
+        <tr>
+          <td colspan="3" class="bb-none bl-none text-right">Subtotal</td>
+          <td class="bb-none text-right"><span class="currency">$</span>[INVOICE_SUBTOTAL]</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="bb-none bt-none bl-none text-right">Tax</td>
+          <td class="bb-none bt-none text-right"><span class="currency">$</span>-</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="bb-none bt-none bl-none text-right">Expenses</td>
+          <td class="bb-none bt-none text-right"><span class="currency">$</span>-</td>
+        </tr>
+        <tr>
+          <td colspan="3" class="bb-none bt-none bl-none text-right">Total</td>
+          <td class="text-right invert-color"><span class="currency">$</span>[INVOICE_TOTAL]</td>
+        </tr>
+      </table>
+    </div>
+    <div class="col-sm-10 mt-2">
+      <p class="text-center text-small">
+        Please process all payments within <span class="bold">15 days</span> of receiving this invoice.
+      </p>
+      <ul>
+        <li class="thin">Make all cheques payable to <span class="bold">Nafeu Nasir</span></li>
+        <li class="thin">eTransfers and Paypal payments are also accepted, please send to  <span class="bold">nafeu.nasir@gmail.com</span></li>
+      </ul>
+    </div>
+    <div class="col-sm-4">
+      <div class="sep"></div>
+    </div>
+    <div class="col-sm-10">
+      <p class="text-center text-small">
+        If you have any questions about this invoice, please call +1(416)894-5354 or email <em>nafeu.nasir@gmail.com</em>
+      </p>
+    </div>
+  </div>
+</body>
+</html>
+"""
 
 parser = argparse.ArgumentParser(description='Generate invoices.')
 parser.add_argument('customer_id',
@@ -91,30 +263,28 @@ def list_customers(config):
 
 
 def build_pdf(config, data, export_path):
-    with open("templates/index.html", "r") as file:
-        print("Generating...")
-        processed_data = process_invoice_data(data)
-        customer = get_customer_by_id(config['customers'], data['customer_id'])
-        template = file.read()
-        template = template.replace("[BUSINESS_NAME]", config['name']) \
-                           .replace("[BUSINESS_ADDRESS]", config['address']) \
-                           .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
-                           .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
-                           .replace("[BUSINESS_PHONE]", config['phone']) \
-                           .replace("[CUSTOMER_NAME]", customer['name']) \
-                           .replace("[CUSTOMER_ID]", str(customer['id'])) \
-                           .replace("[CUSTOMER_ADDRESS]", customer['address']) \
-                           .replace("[CUSTOMER_CITY_PROVINCE_COUNTRY]", customer['city_province_country']) \
-                           .replace("[CUSTOMER_POSTAL_CODE]", customer['postal_code']) \
-                           .replace("[INVOICE_DATE]", data['invoice_date']) \
-                           .replace("[INVOICE_NUMBER]", data['invoice_number']) \
-                           .replace("[INVOICE_TYPE]", data['invoice_type'], 2) \
-                           .replace("[INVOICE_ITEMS]", get_invoice_items(processed_data)) \
-                           .replace("[INVOICE_SUBTOTAL]", str(get_invoice_subtotal(processed_data))) \
-                           .replace("[INVOICE_TOTAL]", str(get_invoice_total(processed_data)))
-        pdfkit.from_string(template, export_path)
-        subprocess.check_call(["open", "-a", "Preview.app", export_path])
-        print("Complete.")
+    print("Generating...")
+    processed_data = process_invoice_data(data)
+    customer = get_customer_by_id(config['customers'], data['customer_id'])
+    template = DEFAULT_TEMPLATE.replace("[BUSINESS_NAME]", config['name']) \
+        .replace("[BUSINESS_ADDRESS]", config['address']) \
+        .replace("[BUSINESS_CITY_PROVINCE_COUNTRY]", config['city_province_country']) \
+        .replace("[BUSINESS_POSTAL_CODE]", config['postal_code']) \
+        .replace("[BUSINESS_PHONE]", config['phone']) \
+        .replace("[CUSTOMER_NAME]", customer['name']) \
+        .replace("[CUSTOMER_ID]", str(customer['id'])) \
+        .replace("[CUSTOMER_ADDRESS]", customer['address']) \
+        .replace("[CUSTOMER_CITY_PROVINCE_COUNTRY]", customer['city_province_country']) \
+        .replace("[CUSTOMER_POSTAL_CODE]", customer['postal_code']) \
+        .replace("[INVOICE_DATE]", data['invoice_date']) \
+        .replace("[INVOICE_NUMBER]", data['invoice_number']) \
+        .replace("[INVOICE_TYPE]", data['invoice_type'], 2) \
+        .replace("[INVOICE_ITEMS]", get_invoice_items(processed_data)) \
+        .replace("[INVOICE_SUBTOTAL]", str(get_invoice_subtotal(processed_data))) \
+        .replace("[INVOICE_TOTAL]", str(get_invoice_total(processed_data)))
+    pdfkit.from_string(template, export_path)
+    subprocess.check_call(["open", "-a", "Preview.app", export_path])
+    print("Complete.")
 
 
 def open_file(path):
@@ -160,13 +330,13 @@ def get_invoice_date(now):
 def get_invoice_items(data):
     out = ""
     for item in data['items']:
-        out += "<tr><td>%s</td>" % item['desc']
-        out += "<td class='text-right'>%s</td>" % item['hours']
+        out += "<tr><td class='item-cell'>%s</td>" % item['desc']
+        out += "<td class='text-center item-cell'>%s</td>" % item['hours']
         rate_label = "/hr"
         if "type" in item:
             rate_label = " (%s)" % item['type']
-        out += "<td class='text-right'><span class='currency'>$</span>%s%s</td>" % (item['rate'], rate_label)
-        out += "<td class='text-right'><span class='currency'>$</span>%s</td></tr>" % item['total']
+        out += "<td class='text-right item-cell'><span class='currency'>$</span>%s%s</td>" % (item['rate'], rate_label)
+        out += "<td class='text-right item-cell'><span class='currency'>$</span>%s</td></tr>" % item['total']
     return out
 
 
@@ -187,7 +357,6 @@ def process_invoice_data(data):
         else:
             item['total'] = item['hours'] * item['rate']
     return data
-
 
 if __name__ == '__main__':
     main()
